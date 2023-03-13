237b10ab425e0d94170ac6ec2a99a0aedd659ace
Alena Anishchenko <alena.anishchenko@gmail.com>
2023-03-13 17:26:55 +0300

    first commit

---
+++ src/main/java/com/javarush/domain/Country.java
@@ -0,0 +1,198 @@
+package com.javarush.domain;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
+@Table(schema = "world", name = "country")
+
+public class Country {
+    @Id
+    @Column(name = "id")
+    private Integer id;
+
+    private String code;
+
+    @Column(name = "code_2")
+    private String alternativeCode;
+
+    private String name;
+
+    @Column(name = "continent")
+    @Enumerated(EnumType.ORDINAL)
+    private Continent continent;
+
+    private String region;
+
+    @Column(name = "surface_area")
+    private BigDecimal surfaceArea;
+
+    @Column(name = "indep_year")
+    private Short independenceYear;
+
+    private Integer population;
+
+    @Column(name = "life_expectancy")
+    private BigDecimal lifeExpectancy;
+
+    @Column(name = "gnp")
+    private BigDecimal GNP;
+
+    @Column(name = "gnpo_id")
+    private BigDecimal GNPOId;
+
+    @Column(name = "local_name")
+    private String localName;
+
+    @Column(name = "government_form")
+    private String governmentForm;
+
+    @Column(name = "head_of_state")
+    private String headOfState;
+
+    @OneToOne
+    @JoinColumn(name = "capital")
+    private City city;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "country_id")
+    private Set<CountryLanguage> languages;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getAlternativeCode() {
+        return alternativeCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Continent getContinent() {
+        return continent;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public BigDecimal getSurfaceArea() {
+        return surfaceArea;
+    }
+
+    public Short getIndependenceYear() {
+        return independenceYear;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public BigDecimal getLifeExpectancy() {
+        return lifeExpectancy;
+    }
+
+    public BigDecimal getGNP() {
+        return GNP;
+    }
+
+    public BigDecimal getGNPOId() {
+        return GNPOId;
+    }
+
+    public String getLocalName() {
+        return localName;
+    }
+
+    public String getGovernmentForm() {
+        return governmentForm;
+    }
+
+    public String getHeadOfState() {
+        return headOfState;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public Set<CountryLanguage> getLanguages() {
+        return languages;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setAlternativeCode(String alternativeCode) {
+        this.alternativeCode = alternativeCode;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContinent(Continent continent) {
+        this.continent = continent;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public void setSurfaceArea(BigDecimal surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
+
+    public void setIndependenceYear(Short independenceYear) {
+        this.independenceYear = independenceYear;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+
+    public void setLifeExpectancy(BigDecimal lifeExpectancy) {
+        this.lifeExpectancy = lifeExpectancy;
+    }
+
+    public void setGNP(BigDecimal GNP) {
+        this.GNP = GNP;
+    }
+
+    public void setGNPOId(BigDecimal GNPOId) {
+        this.GNPOId = GNPOId;
+    }
+
+    public void setLocalName(String localName) {
+        this.localName = localName;
+    }
+
+    public void setGovernmentForm(String governmentForm) {
+        this.governmentForm = governmentForm;
+    }
+
+    public void setHeadOfState(String headOfState) {
+        this.headOfState = headOfState;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public void setLanguages(Set<CountryLanguage> languages) {
+        this.languages = languages;
+    }
+}
---
+++ src/main/java/com/javarush/domain/CountryLanguage.java
@@ -0,0 +1,67 @@
+package com.javarush.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Type;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(schema = "world", name = "country_language")
+public class CountryLanguage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    private String language;
+
+    @Column(name = "is_official", columnDefinition = "BIT")
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    private Boolean isOfficial;
+
+    private BigDecimal percentage;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public Boolean getOfficial() {
+        return isOfficial;
+    }
+
+    public BigDecimal getPercentage() {
+        return percentage;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void setOfficial(Boolean official) {
+        isOfficial = official;
+    }
+
+    public void setPercentage(BigDecimal percentage) {
+        this.percentage = percentage;
+    }
+}
